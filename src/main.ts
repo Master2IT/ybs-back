da971ca7bd643068599434f3b655fcb065b849e6
Mehran Mahdavi <mehran73master@gmail.com>
2023-08-02 12:45:09 +0300

    allow cros origin

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.setGlobalPrefix('api');
 
   await app.listen(5000);
